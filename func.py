f72873050954d95d4b52d8d158a5cf9d0c4ee404
yslin2015 <359927530@qq.com>
2017-11-29 19:39:10 +0800

    初始化

---
+++ func.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def person(name, age=20, **other):
+	print('name:', name, 'age:', age, 'kw:', other)
+
+
+# person('Jack', 25)
+# person('Jack', 35)
+# person('Jack', city='Beijing', height=170)
+
+
+b =	range(100)
+print(b[:20:5])
+c = [1, 7, 'ABC']
+
+# a = {'a': 1, 'b': 2}
+
+
+
+
+
+
+
+
+
+
+# print('hello, python')
